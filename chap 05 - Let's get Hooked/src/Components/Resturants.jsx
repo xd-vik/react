ba923336d71c0f5292170e3ad0b43b0e89f67ad5
meow :) <89735040+xd-vik@users.noreply.github.com>
2024-03-17 23:10:41 +0530

    Seach fn Added

--- chap 05 - Let's get Hooked/src/Components/Resturants.jsx
+++ chap 05 - Let's get Hooked/src/Components/Resturants.jsx
@@ -1,10 +1,12 @@
-import React, { createElement, useState } from 'react'
+import React, { createElement, useEffect, useState } from 'react'
 import Cards from './Cards'
 import { Resturant } from '../constant'
+import SimEffect from './simEffect';
 
-function filterData(search,Restraun){
+
+ function filterData(search,Restraun){
      const filterData =  Restraun.filter((res)=>
-        res.info.name.includes(search)
+        res.info.name.toLowerCase().includes(search.toLowerCase())
      
     )
 return filterData;
@@ -13,8 +15,31 @@ return filterData;
 const Resturants = () => {
     const [search, getSearch] = useState("Pizza");
     const [data,setData] = useState("");
-    const [Restraun, setRestraun] = useState(Resturant);
-    return(
+    const [Restraun, setRestraun] = useState([]);
+    const [filterRes , setFilterRes ] = useState([]);
+
+   useState(
+    getResturants
+   )
+
+    async function getResturants(){
+        const data =  await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.59080&lng=85.13480&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const json = await data.json();
+        
+        setRestraun(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setFilterRes(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+    }
+
+    if(!Restraun) return null;
+
+    function sync (){
+        if(filterRes?.length ===    0){
+           return <h1>Not matched with {search} </h1>
+        }
+        else <span> Results for  {search} </span>
+    }
+
+    return (Restraun.length === 0 ? ( <SimEffect/> ) :(
         <>
         <div className="search">
             <input 
@@ -30,16 +55,17 @@ const Resturants = () => {
             />
             <button onClick={
                 ()=>{
+                    
                     const Resdata = filterData(search, Restraun)
-                    setRestraun(Resdata);
-                  setData(  " Results for  " + search  )
+                    setFilterRes(Resdata);
+                    setData(sync());
                 }
             } >Search</button>
             <div>  {data} </div>
             </div>  
         <div className="Res">
         {
-            Restraun.map((resturant)=>{
+            filterRes.map((resturant)=>{
                 return <Cards {...resturant.info} key={resturant.info.id}/>
                
             })
@@ -49,7 +75,7 @@ const Resturants = () => {
   
         
     )
-   
+    )
 }
 
 export default Resturants
